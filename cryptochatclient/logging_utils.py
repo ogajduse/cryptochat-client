fff658e136e191f23484671f00fecc05136994a6
Ondřej Gajdušek <ogajduse@redhat.com>
2019-02-21 13:54:47 +0100

    another batch of changes

--- cryptochatclient/logging_utils.py
+++ cryptochatclient/logging_utils.py
@@ -1,11 +1,10 @@
 """
-Common logging functionality to be used for multiple apps
-(should be identical to reposcan/common/logging.py)
-TODO: packaging changes that let this live in one place please
+Common logging functionality
 """
 
 import logging
 import os
+
 
 class OneLineExceptionFormatter(logging.Formatter):
     """
@@ -13,7 +12,6 @@ class OneLineExceptionFormatter(logging.Formatter):
     (insures one entry-per-log for some logging environments that divide on newline)
     """
 
-    # pylint: disable=arguments-differ
     def formatException(self, exc_info):
         """
         Make sure exception-tracebacks end up on a single line.
@@ -30,27 +28,23 @@ class OneLineExceptionFormatter(logging.Formatter):
             fmt_str = fmt_str.replace('\n', '') + '|'
         return fmt_str
 
-def init_logging(num_servers=1):
+
+def init_logging():
     """Setup root logger handler."""
     logger = logging.getLogger()
-    log_type = os.getenv('LOGGING_TYPE', "OPENSHIFT")
-    if log_type == "OPENSHIFT":
-        log_fmt = "%(name)s: [%(levelname)s] %(message)s"
-    else:
-        uuid = os.uname().nodename
-        if num_servers > 1:
-            uuid += ":%d" % os.getpid()
-        log_fmt = uuid + " %(asctime)s %(name)s: [%(levelname)s] %(message)s"
+    uuid = os.uname().nodename
+    uuid += ":%d" % os.getpid()
+    log_fmt = uuid + " %(asctime)s %(name)s: [%(levelname)s] %(message)s"
     if not logger.handlers:
         handler = logging.StreamHandler()
         formatter = OneLineExceptionFormatter(log_fmt)
         handler.setFormatter(formatter)
         logger.addHandler(handler)
 
+
 def get_logger(name):
     """
     Set logging level and return logger.
-    Don't set custom logging level in root handler to not display debug messages from underlying libraries.
     """
     logger = logging.getLogger(name)
     level = os.getenv('LOGGING_LEVEL', "INFO")
